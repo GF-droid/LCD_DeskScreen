29f4cec28a5a081a3d87d6ff6b8b130ac7f702f7
xiaoxing <19151564082@163.com>
2026-07-17 11:27:23 +0800

    add demo3 read

--- study/test_demo3/test_write.c
+++ study/test_demo3/test_write.c
@@ -2,15 +2,48 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
+
+size_t SIZEBUFFER = 100;
 
 int main() {
   char* buf = "Hello, World!\n";
-  int fd = open("file.txt", O_WRONLY | O_CREAT, 0644);
+  char* read_buf = malloc(sizeof(char) * SIZEBUFFER);
+  if (!read_buf) {
+    perror("malloc");
+    _exit(1);
+  }
+
+  int fd = open("file.txt", O_RDWR | O_CREAT, 0644);
   if (fd == -1) {
     perror("open");
     _exit(1);
   }
-  write(fd, buf, strlen(buf));
+
+  ssize_t bytes_written = write(fd, buf, strlen(buf));
+  printf("Wrote %zd bytes: %zd\n", bytes_written, strlen(buf));
+  if (bytes_written == -1) {
+    perror("write");
+    _exit(1);
+  }
+
+  off_t pos = lseek(fd, 0, SEEK_SET);
+  if (pos == -1) {
+    perror("lseek");
+    _exit(1);
+  }
+
+  ssize_t bytes_read = read(fd, read_buf, SIZEBUFFER - 1);
+  if (bytes_read == -1) {
+    perror("read");
+    _exit(1);
+  }
+
+  read_buf[bytes_read] = '\0'; // Null-terminate the read buffer
+
+  printf("Read %zd bytes: %.*s\n", bytes_read, (int)bytes_read, read_buf);
+
   close(fd);
+  free(read_buf);
   return 0;
 }
